9d32014812ce14a3dcf9d352a07cafe0091260de
zvereva <96833638+zvereva-s@users.noreply.github.com>
2022-06-03 21:46:33 +0300

    sum

---
+++ src/index.js
@@ -0,0 +1,54 @@
+//! ******* all imports ******* //
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
+import countryDesc from './templates/countryDesc.hbs';
+import countriesList from './templates/countriesList.hbs';
+import { fetchCountries } from './js/fetchCountries.js';
+import './css/styles.css';
+
+
+//! ******* all global variables ******* //
+const DEBOUNCE_DELAY = 300;
+
+const inputEl = document.querySelector('#search-box');
+const listOfCountries = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+
+//** start of coding */
+inputEl.addEventListener('input', debounce(onInputChange, DEBOUNCE_DELAY));
+function onInputChange(e) {
+    e.preventDefault();
+    const searchName = inputEl.value.trim();
+
+    if (searchName === '') {
+        listOfCountries.innerHTML = '';
+        return;
+    }    
+    fetchCountries(searchName).then(result => {
+        checkResult(result)}).catch((err) => {
+        console.log(err);
+            Notify.failure('Oops, there is no country with that name');
+    })
+
+ };
+
+function checkResult(result) {
+    if (result.length > 10) {
+        listOfCountries.innerHTML = '';
+        Notify.info("Too many matches found. Please enter a more specific name.");
+        return;
+    } else if (result.length === 1) {
+        result[0].languages = [...Object.values(result[0].languages)];
+        result[0].capital = result[0].capital.join('');
+
+        console.log(result[0]);
+
+        countryInfo.innerHTML = countryDesc({name, capital, population, languages}=result[0]);
+        listOfCountries.innerHTML = '';
+    } else {
+        listOfCountries.innerHTML = countriesList(result);
+        countryInfo.innerHTML = '';
+    }
+ };
+
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,12 @@
+const BASE_URL = 'https://restcountries.com/v3.1';
+const SEARCH_PARAM = 'fields=name,capital,population,flags,languages';
+
+export function fetchCountries(name) {
+return fetch(`${BASE_URL}/name/${name}?${SEARCH_PARAM}`)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+};
